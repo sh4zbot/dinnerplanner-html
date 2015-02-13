86589a4feb7f4e6b2f5e90438910bd816eb8ed47
Johan Lindeberg <jlindebe@kth.se>
2015-02-13 13:19:44 +0100

    added app folder

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(function() {
 	console.info("Alou_testando");
 	//And create the needed controllers and views
 	//var exampleView = new ExampleView($("#exampleView"));
-	var exampleView = new ExampleView($("#exampleView"), model);
+	//var exampleView = new ExampleView($("#exampleView"), model);
 
 	//var dishdetailView = new DishDetailView($( "#dishView" ).load( "screen3-lasagne.html #dishdetailView" );, model);
 	
